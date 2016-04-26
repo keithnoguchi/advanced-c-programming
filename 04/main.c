032dccac847bcee723318851ad25dcff8533b53d
Kei Nohguchi <kei@nohguchi.com>
2016-04-26 02:07:55 -0700

    main.c: Cleanup the tab from the comment line.

--- 04/main.c
+++ 04/main.c
@@ -16,32 +16,32 @@
 
    Objective: Read the student scores for 4 to 6 students from different
               exams as input from a input file, the scores as follows:
-	      91, 92, 85, 58, 87, 75, 89, 97, 79, 65, 88, 72, 81, 94, 90, 61,
-	      72, 75, 68, 77, 75, 49, 87, 79, 65, 64, 62, 51, 44, 70, 81, 72,
-	      85, 78, 77, 75, 79, 87, 69, 55, 88, 62, 71, 74, 80, 71, 62, 85,
-	      68, 87, 75, 89, 97, 79, 65, 48, 72, 61, 64, 90
+              91, 92, 85, 58, 87, 75, 89, 97, 79, 65, 88, 72, 81, 94, 90, 61,
+              72, 75, 68, 77, 75, 49, 87, 79, 65, 64, 62, 51, 44, 70, 81, 72,
+              85, 78, 77, 75, 79, 87, 69, 55, 88, 62, 71, 74, 80, 71, 62, 85,
+              68, 87, 75, 89, 97, 79, 65, 48, 72, 61, 64, 90
 
-	      Call functions to find the average, minimum, and maximum score
-	      for each student.  Call a function to assign the student number
-	      using a static variable for the student number which will
-	      automatically increment whenever the function is called.
-	      Show the student number along with the statistics for the
-	      scores.  Find the average, minimum, and maximum for all
-	      students.  Print all scores for each student.  Write all the
-	      output to a file.  Assign a letter grade for each score
-	      and print.
+              Call functions to find the average, minimum, and maximum score
+              for each student.  Call a function to assign the student number
+              using a static variable for the student number which will
+              automatically increment whenever the function is called.
+              Show the student number along with the statistics for the
+              scores.  Find the average, minimum, and maximum for all
+              students.  Print all scores for each student.  Write all the
+              output to a file.  Assign a letter grade for each score
+              and print.
 
-	      Grading Policy:
+              Grading Policy:
 
-	      A+: >  95
+              A+: >  95
               A:     95
-	      A-: >= 90
-	      B+: >  85
+              A-: >= 90
+              B+: >  85
               B:     85
-	      B-: >= 80
-	      C+: >  75
+              B-: >= 80
+              C+: >  75
               C:     75
-	      C-: >= 70
+              C-: >= 70
               D:  >= 60
               F:  <  60
 
