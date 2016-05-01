a549a28627217eab3d857ca82ba3b121b1b7f8d8
Kei Nohguchi <kei@nohguchi.com>
2016-05-01 00:09:08 -0700

    05/main.c: Cleanup the subject of the print out contents

--- 05/main.c
+++ 05/main.c
@@ -48,30 +48,30 @@ int main()
 	char *cptr = &c;
 	int **iptrptr = &iptr;
 
-	printf("Print out the value itself\n");
-	printf("--------------------------\n");
-	printf("%-45s %d\n", "Integer i:", i);
-	printf("%-45s %f\n", "Float f:", f);
-	printf("%-45s %F\n", "Double d:", d);
-	printf("%-45s %s\n", "String s:", s);
-	printf("%-45s %c\n", "Character c:", c);
+	printf("Actual value\n");
+	printf("------------\n");
+	printf("%-55s %d\n", "Integer i:", i);
+	printf("%-55s %f\n", "Float f:", f);
+	printf("%-55s %F\n", "Double d:", d);
+	printf("%-55s %s\n", "String s:", s);
+	printf("%-55s %c\n", "Character c:", c);
 
-	printf("\nPrint out the addresses of the value, using the pointers\n");
+	printf("\nAddresses of the value, using the corresponding pointers\n");
 	printf("--------------------------------------------------------\n");
-	printf("%-45s %p\n", "Address of integer i:", iptr);
-	printf("%-45s %p\n", "Address of float f:", fptr);
-	printf("%-45s %p\n", "Address of double d:", dptr);
-	printf("%-45s %p\n", "Address of string s:", sptr);
-	printf("%-45s %p\n", "Address of character c:", cptr);
-	printf("%-45s %p\n", "Address of pointer to integer i:", iptrptr);
+	printf("%-55s %p\n", "Address of integer i:", iptr);
+	printf("%-55s %p\n", "Address of float f:", fptr);
+	printf("%-55s %p\n", "Address of double d:", dptr);
+	printf("%-55s %p\n", "Address of string s:", sptr);
+	printf("%-55s %p\n", "Address of character c:", cptr);
+	printf("%-55s %p\n", "Address of pointer to integer i:", iptrptr);
 
-	printf("\nPrint the address of the pionters\n");
-	printf("---------------------------------\n");
-	printf("%-45s %p\n", "Address of the integer pionter iptr:", &iptr);
-	printf("%-45s %p\n", "Address of the float pointer fptr:", &fptr);
-	printf("%-45s %p\n", "Address of the double pinter dptr:", &dptr);
-	printf("%-45s %p\n", "Address of the string s pointer sptr:", &sptr);
-	printf("%-45s %p\n", "Address of the character c pinter cptr:", &cptr);
-	printf("%-45s %p\n", "Address of the pointer to integer i iptrptr:",
+	printf("\nAddress of the pionters\n");
+	printf("-----------------------\n");
+	printf("%-55s %p\n", "Address of the integer pionter iptr:", &iptr);
+	printf("%-55s %p\n", "Address of the float pointer fptr:", &fptr);
+	printf("%-55s %p\n", "Address of the double pinter dptr:", &dptr);
+	printf("%-55s %p\n", "Address of the string s pointer sptr:", &sptr);
+	printf("%-55s %p\n", "Address of the character c pinter cptr:", &cptr);
+	printf("%-55s %p\n", "Address of the pointer to integer i iptrptr:",
 			&iptrptr);
 }
