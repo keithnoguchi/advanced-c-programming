ea0899a1e9e34668ceb2268fa6e2fc4de1842d4f
Kei Nohguchi <kei@nohguchi.com>
2016-05-01 19:09:31 -0700

    06/main.c: Check column and row in the brute force way

--- 06/main.c
+++ 06/main.c
@@ -17,47 +17,52 @@
 */
 
 #include <stdio.h>
+#include <stdbool.h>
 
-static const size_t column = 4;
-static const size_t row = 4;
-static char grid[4][4];
+#define GRID_SIZE   4
+static char grid[GRID_SIZE][GRID_SIZE];
 
 static void init_grid(void)
 {
 	int i, j;
 
-	for (i = 0; i < row; ++i)
-		for (j = 0; j < column; ++j)
+	for (i = 0; i < GRID_SIZE; ++i)
+		for (j = 0; j < GRID_SIZE; ++j)
 			grid[i][j] = '-';
 }
 
 static void print_title(void)
 {
-	printf("\nTic-tac-toe 4x4 version\n");
+	printf("\nTic-tac-toe %dx%d version\n", GRID_SIZE, GRID_SIZE);
 }
 
 static void print_grid(void)
 {
 	int i, j;
 
-	printf("\n\t  1 2 3 4 x\n");
-	for (i = 0; i < row; ++i) {
+	printf("\n\t  ");
+	for (i = 0; i < GRID_SIZE; ++i)
+		printf("%d ", i + 1);
+	printf("x\n");
+
+	for (i = 0; i < GRID_SIZE; ++i) {
 		printf("\t%d ", i + 1);
-		for (j = 0; j < column; ++j)
+		for (j = 0; j < GRID_SIZE; ++j)
 			printf("%c ", grid[i][j]);
 		printf("\n");
 	}
+
 	printf("\ty\n\n");
 }
 
 static int print_prompt(void)
 {
 	print_grid();
-	printf("Next position in x, y or ^C to quit. > ");
+	printf("Next position in x, y or ^C to quit: ");
 	return 1;
 }
 
-static int get_input(unsigned *x, unsigned *y)
+static int get_position(unsigned *x, unsigned *y)
 {
 	int ret;
 
@@ -65,33 +70,111 @@ static int get_input(unsigned *x, unsigned *y)
 	if (ret != 2)
 		return EOF;
 
-	if (*x <= 0 || *x > column) {
-		printf("x should be 1 <= x <= %d\n", column);
+	if (*x <= 0 || *x > GRID_SIZE) {
+		printf("x should be 1 <= x <= %d\n", GRID_SIZE);
 		*x = 0;
 	}
-	if (*y <= 0 || *y > row) {
-		printf("y should be 1 <= y <= %d\n", row);
+	if (*y <= 0 || *y > GRID_SIZE) {
+		printf("y should be 1 <= y <= %d\n", GRID_SIZE);
 		*y = 0;
 	}
 
 	return ret;
 }
 
-static void fill_grid(const int x, const int y, const char role)
+static bool is_grid_empty(const int x, const int y)
 {
-	grid[y - 1][x - 1] = role;
+	return grid[y - 1][x - 1] == '-';
+}
+
+static bool is_valid_position(const int x, const int y)
+{
+	if (x == 0 || y == 0 || !is_grid_empty(x, y)) {
+		printf("Invalid position. Try again.\n");
+		return false;
+	} else
+		return true;
+}
+
+static void fill_grid(const int x, const int y, const char player)
+{
+	grid[y - 1][x - 1] = player;
+}
+
+static int check_column(const int x, const int y, const char player)
+{
+	int count, prev_count;
+	int i;
+
+	count = prev_count = 0;
+	for (i = 0; i < GRID_SIZE; ++i) {
+		if (grid[i][x - 1] == player)
+			++count;
+		else {
+			if (count > prev_count)
+				prev_count = count;
+			count = 0;
+		}
+	}
+	return prev_count > count ? prev_count : count;
+}
+
+static int check_row(const int x, const int y, const char player)
+{
+	int count, prev_count;
+	int i;
+
+	count = prev_count = 0;
+	for (i = 0; i < GRID_SIZE; ++i) {
+		if (grid[y - 1][i] == player)
+			++count;
+		else {
+			if (count > prev_count)
+				prev_count = count;
+			count = 0;
+		}
+	}
+	return prev_count > count ? prev_count : count;
+}
+
+static bool is_won(const int x, const int y, const char player)
+{
+	const int min_win_count = GRID_SIZE - 1;
+	int count;
+
+	count = check_column(x, y, player);
+	if (count >= min_win_count)
+		return true;
+
+	count = check_row(x, y, player);
+	if (count >= min_win_count)
+		return true;
+
+	return false;
 }
 
 int main()
 {
-	const char role_char[] = {'O', 'X'};
+	const char player_sym[] = {'X', 'O'};
 	unsigned x, y;
-	int i;
+	int count;
 
 	init_grid();
 	print_title();
-	i = 0;
-	for (print_prompt(); get_input(&x, &y) != EOF; print_prompt())
-		if (x != 0 && y != 0)
-			fill_grid(x, y, role_char[i++ % 2]);
+
+	count = 0;
+	for (print_prompt(); get_position(&x, &y) != EOF; print_prompt())
+		if (is_valid_position(x, y)) {
+			char player = player_sym[count++ % 2];
+			fill_grid(x, y, player);
+			if (is_won(x, y, player)) {
+				printf("%c won!\n", player);
+				print_grid();
+				break;
+			} else if (count == GRID_SIZE * GRID_SIZE) {
+				printf("Draw.\n");
+				print_grid();
+				break;
+			}
+		}
 }
