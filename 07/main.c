d1c18ea7fab252b15f8e73d55b74d7167146b779
Kei Nohguchi <kei@nohguchi.com>
2016-05-14 17:44:35 -0700

    07/main.c: Get the first column number from the stdin

--- 07/main.c
+++ 07/main.c
@@ -18,23 +18,90 @@
 */
 
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <ctype.h>
 
+static const int max_row = 8;
+static const int max_column = 8;
+
+static char board[8][8] = {
+	{'*', '*', '*', '*', '*', '*', '*', '*'},
+	{'*', '*', '*', '*', '*', '*', '*', '*'},
+	{'*', '*', '*', '*', '*', '*', '*', '*'},
+	{'*', '*', '*', '*', '*', '*', '*', '*'},
+	{'*', '*', '*', '*', '*', '*', '*', '*'},
+	{'*', '*', '*', '*', '*', '*', '*', '*'},
+	{'*', '*', '*', '*', '*', '*', '*', '*'},
+	{'*', '*', '*', '*', '*', '*', '*', '*'}
+};
+
+static void xprintf(FILE *os, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	va_end(ap);
+}
+
+static void print_board(FILE *os)
+{
+	int i, j;
+
+	for (i = 0; i < max_row; ++i) {
+		xprintf(os, "\t");
+		for (j = 0; j < max_column; ++j)
+			xprintf(os, "%c ", board[i][j]);
+		xprintf(os, "\n");
+	}
+	xprintf(os, "\n");
+}
+
+static void prompt(FILE *os)
+{
+	xprintf(os, "Which column(0-7) do you want to put your "
+		"first queen or Q for quit: ");
+}
+
+static int input(FILE *is)
+{
+	int column = max_column;
+	char c, nl;
+	int ret;
+
+	ret = scanf("%c%c", &c, &nl);
+	if (ret == EOF || (isalpha(c) && toupper(c) == 'Q'))
+		return EOF;
+	else if (isdigit(c)) {
+		char str[BUFSIZ];
+
+		str[0] = c;
+		str[1] = '\0';
+		column = atoi(str);
+		if (column < 0 || column >= max_column)
+			column = max_column;
+	}
+	return column;
+}
+
 int main()
 {
-	char c, nl;
+	FILE *is = NULL;
+	FILE *os = NULL;
 	int ret;
 
 	printf("Eight queen problem\n\n");
 
-	while (true) {
-		printf("First queen location in digit (0-7) or Q for quit: ");
-		ret = scanf("%c%c", &c, &nl);
-		if (ret == EOF || (isalpha(c) && toupper(c) == 'Q'))
-			break;
-		printf("%c%c", c, nl);
+	for (prompt(os); (ret = input(is)) != EOF; prompt(os)) {
+		if (ret >= max_column) {
+			xprintf(os, "Column number should be between 0 to 7\n");
+			continue;
+		}
+		xprintf(os, "%d\n", ret);
+		print_board(NULL);
 	}
 
-	printf("\nThank you!\n");
+	xprintf(os, "\nThank you!\n");
 }
