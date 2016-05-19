6f2139d4092fe2820c2f25a8f5e4167ab9ad5ff4
Kei Nohguchi <kei@nohguchi.com>
2016-05-19 00:01:11 -0700

    10/main.c: Tower of Hanoi!

---
+++ 10/main.c
@@ -0,0 +1,28 @@
+/*
+   University of California Extension, Santa Cruz
+
+   Advanced C Programming
+
+   Instructor: Rajainder A.
+   Author: Kei Nohguchi
+   Assignment Number: 10
+
+   Topic: Tower of Hanoi
+
+   file name: main.c
+
+   Date: May 18th, 2016
+
+   Objective: Move 4, 5, or 6 disks from 'from' post to 'to' post,
+   by using the middle 'auxiliary' post.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Tower of Hanoi\n");
+	printf("==============\n");
+
+	printf("Thank you!\n");
+}
