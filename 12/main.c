df4b0dd6d82090eab22d176d2c8a049e58418c7d
Kei Nohguchi <kei@nohguchi.com>
2016-05-25 06:23:11 -0700

    12/main.c: sorting!

---
+++ 12/main.c
@@ -0,0 +1,24 @@
+/*
+   University of California Extension, Santa Cruz
+
+   Advanced C Programming
+
+   Instructor: Rajainder A.
+   Author: Kei Nohguchi
+   Assignment Number: 12
+
+   Topic: sorting
+
+   file name: main.c
+
+   Date: May 25th, 2016
+
+   Objective: */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello world!\n");
+	return 0;
+}
