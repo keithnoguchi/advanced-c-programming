3c4016b9d92b03ec03e0b08cbb39b3c1debcf023
Kei Nohguchi <kei@nohguchi.com>
2016-05-25 06:55:03 -0700

    12/main.c: Read array input from the file, input.txt

--- 12/main.c
+++ 12/main.c
@@ -13,12 +13,44 @@
 
    Date: May 25th, 2016
 
-   Objective: */
+   Objective: Implement four sinple sort routines, bubble, insert,
+              selection, and shell, as well as three advanced sort
+	      routines, quick, heap, and merge sorts.  Get the array
+              of input from the input file, input.txt, and ask the
+              user to select one for each simple and advanced sort
+              routine.  The result of the sorted array, done by
+              two selected sort routine, will be displayed on the
+              console, as well as saved into the output file, output.txt.
+
+   Input array: 49, 17, 56, 85, 12, 97, 33, 71, 19, 62, 38, 84, 51,
+                29, 77, 65, 44, 99,  4, 47, 67, 41, 23, 88, 73,  8,
+               100, 25, 91, 58, 59, 22, 15, 35, 95, 60, 20,  7, 50,
+                10
+              */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	printf("Hello world!\n");
-	return 0;
+	const char *input_file = "input.txt";
+	const char *output_file = "output.txt";
+	FILE *is = NULL, *os = NULL;
+	int ret = EXIT_SUCCESS;
+
+	is = fopen(input_file, "r");
+	if (is == NULL) {
+		fprintf(stderr, "can't open '%s' for reading.\n",
+				input_file);
+		ret = EXIT_FAILURE;
+		goto err;
+	}
+
+err:
+	if (os)
+		fclose(os);
+	if (is)
+		fclose(is);
+
+	exit(ret);
 }
