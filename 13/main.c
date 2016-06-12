43f3b0f0f9c08f2d6840c78116feb266b839e31f
Kei Nohguchi <kei@nohguchi.com>
2016-06-12 14:54:51 -0700

    13/main.c: Add objective as well as the input value

--- 13/main.c
+++ 13/main.c
@@ -13,9 +13,21 @@
 
    Date: May 12th, 2016
 
-   Objective: 
+   Objective: Read the following numbers from an input file directory
+              as integer values in to a binary tree.  Using a binary
+              tree structure read the number and form an inordered tree.
+              All nodes to left subtree of the root will be lower than
+              root and all nodes in the right subtree are higher.
+              Traverse the tree in preorder, inorder, postorder, and
+              print the data value (info) of the node when the node is
+              visited.  Write delete algorithm to delete nodes and
+              replacing them with their inorder successor.  Print value
+              inorder after all deletes.  Write both non-recursive and
+              recursive routines to traverse the trees.
 
-   Input array:
+   Input array:  55, 62, 89, 85, 97, 56, 71, 82, 38, 49, 25, 67, 58, 92,
+                100, 44, 69, 72, 65, 52, 41, 84, 21, 60, 95, 12,
+                 35, 42, 105, 99, 34, 47, 35, 79, 95, 50, 25, 51
 */
 
 #include <stdio.h>
