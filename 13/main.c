743fe02aac14ff3405ab418b2c4690ed9486706d
Kei Nohguchi <kei@nohguchi.com>
2016-06-12 14:46:52 -0700

    13/main.c: binary tree traversal

---
+++ 13/main.c
@@ -0,0 +1,27 @@
+/*
+   University of California Extension, Santa Cruz
+
+   Advanced C Programming
+
+   Instructor: Rajainder A.
+   Author: Kei Nohguchi
+   Assignment Number: 13
+
+   Topic: binary tree traversal
+
+   file name: main.c
+
+   Date: May 12th, 2016
+
+   Objective: 
+
+   Input array:
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello world!\n");
+}
+
