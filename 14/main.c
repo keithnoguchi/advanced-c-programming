5fa79ada3ff3ccc787cb118eec4df3c56732452c
Kei Nohguchi <kei@nohguchi.com>
2016-06-20 14:59:42 -0700

    14/main.c: B-tree implementation

---
+++ 14/main.c
@@ -0,0 +1,27 @@
+/*
+   University of California Extension, Santa Cruz
+
+   Advanced C Programming
+
+   Instructor: Rajainder A.
+   Author: Kei Nohguchi
+   Assignment Number: 14
+
+   Topic: B-tree
+
+   file name: main.c
+
+   Date: May 20th, 2016
+
+   Objective: 
+
+   Input array:
+
+*/
+
+#include <stdio.h>
+
+void main()
+{
+	printf("Hello world\n");
+}
