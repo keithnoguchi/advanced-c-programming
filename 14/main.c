ff341da81794d8768786bb415bee78fe6f84c782
Kei Nohguchi <kei@nohguchi.com>
2016-06-20 15:21:47 -0700

    14/main.c: Add objective and the array input data in the header

--- 14/main.c
+++ 14/main.c
@@ -13,15 +13,53 @@
 
    Date: May 20th, 2016
 
-   Objective: 
+   Objective: Read the following numbers from an input file in the
+              same order. (Note: Do not assign an array with input
+              values.)
 
-   Input array:
+              Using a B-tree structure read the numbers and form
+              the tree.  All keys in the nodes of the left subtree
+              will be lower than parent key and all keys in the right
+              subtree are higher than the parent key.  Traverse the
+              tree in inorder after each split.  Print the input values
+              and the output node key values in inorder to output file.
+
+   Input array: 572, 430, 315, 363, 320, 545, 451, 437, 476, 472,
+                493, 395, 462, 521, 406, 412, 510, 560, 425, 595,
+		580, 583, 531, 511, 459, 518, 356, 379, 488, 532
 
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void main()
 {
+	const char *input = "input.txt", *output = "output.txt";
+	FILE *is = NULL, *os = NULL;
+	int ret = EXIT_SUCCESS;
+
+	is = fopen(input, "r");
+	if (is == NULL) {
+		fprintf(stderr, "can't open '%s' input file.\n", input);
+		ret = EXIT_FAILURE;
+		goto err;
+	}
+
+	os = fopen(output, "w");
+	if (os == NULL) {
+		fprintf(stderr, "can't open '%s' output file.\n", output);
+		ret = EXIT_FAILURE;
+		goto err;
+	}
+
 	printf("Hello world\n");
+
+err:
+	if (os != NULL)
+		fclose(os);
+	if (is != NULL)
+		fclose(is);
+
+	exit(ret);
 }
