45bb0e66f23d81a42fc8a18f89788dbb07bc0965
Kei Nohguchi <kei@nohguchi.com>
2016-06-28 15:30:12 -0700

    10/main.c: Update the prolog to provide more info
    Especially the hint section of the prolog is really important
    to implement a efficient solution.

--- 10/main.c
+++ 10/main.c
@@ -7,14 +7,40 @@
    Author: Kei Nohguchi
    Assignment Number: 10
 
-   Topic: Tower of Hanoi
+   Topic: Tower of Hanoi, the power of recursion
 
    file name: main.c
 
-   Date: May 18th, 2016
+   Date: June 28th, 2016
 
-   Objective: Move 4, 5, or 6 disks from 'from' post to 'to' post,
-   by using the middle 'auxiliary' post.
+   Background: The problem of towers of Hanoi has five, it's
+               arbitrary though, disks of differing graded diameters
+               are placed on peg A, so that a larger disks is always
+               below a smaller disk. The objective is to move the five
+               disks to peg C, using peg B as auxiliary.  Only the top
+               disk on any peg may be moved to any other peg, and
+               a larger disk may never rest on a smaller one.
+
+   Objective: Prompt the user to give the number of disks
+              to be placed on the target post(C) from the initial
+              post(A), using the auxilary post(B).  Three outputs
+              are expected for this assignment, with 4, 5, and 6
+              disks by prompting the user to specify the number
+              of disks within the range.
+
+	      Also, print each move of every disk from any peg
+              to any peg.
+
+    Hint: The solution needs two recursive calls in the main program.
+
+          One is to place n-1 disks recursively from the from pole(A)
+          to the auxiliary pole(B) and place disk n from the from pole(A)
+          to the to pole(C).
+
+          Second one is to place n-1 disks recursively from the auxiliary
+          pole(B) to the to pole(C) using the from pole(A) as auxiliary
+          pole.
+
 */
 
 #include <stdio.h>
