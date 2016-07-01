fd0329c75deb7c86c6f6ede55f08f965465cfe3b
Kei Nohguchi <kei@nohguchi.com>
2016-06-30 22:32:32 -0700

    15/main.c: Hash!

---
+++ 15/main.c
@@ -0,0 +1,31 @@
+/*
+   University of California Extension, Santa Cruz
+
+   Advanced C Programming
+
+   Instructor: Rajainder A.
+   Author: Kei Nohguchi
+   Assignment Number: 15
+
+   Topic: Hash
+
+   file name: main.c
+
+   Date: June 30th, 2016
+
+   Objective: Implement a hash table of size 15.  Use the Separate
+              Chaining for collisions.
+
+              Give your input file of 100 records with keys.  Use
+              the structure of {key, record, next} for the linked
+              nodes.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Here you go!\n");
+	exit(EXIT_SUCCESS);
+}
