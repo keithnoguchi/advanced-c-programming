36422f1230b20ca7f6750f5fd4bc3d020d143be2
Kei Nohguchi <kei@nohguchi.com>
2016-07-02 23:08:24 -0700

    18/main.c: Open up the input and output files

--- 18/main.c
+++ 18/main.c
@@ -13,9 +13,18 @@
 
    Date: July 2nd, 2016
 
-   Background:
+   Objective: Read the following two long integer inputs from
+              an input file in the same order. (Note: Do not
+              assign an array with input values.)
 
-   Objective: 
+              List 1: 954680325257568512973371196238845129776544789456
+
+              List 2: 64324564257936835497210698463523456234
+
+              Using a list structure read the numbers and forms
+              a linear lined list, print list elements as list1
+              and list2.  Consider reading 4 or 5 digits at a time
+              into the node.
 
 */
 
@@ -24,6 +33,29 @@
 
 int main()
 {
+	const char *const input = "input.txt";
+	const char *const output = "output.txt";
+	FILE *is = NULL, *os = NULL;
+
+	is = fopen(input, "r");
+	if (is == NULL) {
+		fprintf(stderr, "can't open %s to read.\n", input);
+		goto err;
+	}
+
+	os = fopen(output, "w");
+	if (os == NULL) {
+		fprintf(stderr, "can't open %s to write.\n", output);
+		goto err;
+	}
+
 	printf("Hello world!\n");
+
+err:
+	if (is != NULL)
+		fclose(is);
+	if (os != NULL)
+		fclose(os);
+
 	exit(EXIT_SUCCESS);
 }
