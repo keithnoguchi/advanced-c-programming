472486cd6410f44d091e6c6167302d8401fcc9d1
Kei Nohguchi <kei@nohguchi.com>
2016-07-02 22:49:24 -0700

    18/main.c: Start to implement the big integer arithmatic

---
+++ 18/main.c
@@ -0,0 +1,29 @@
+/*
+   University of California Extension, Santa Cruz
+
+   Advanced C Programming
+
+   Instructor: Rajainder A.
+   Author: Kei Nohguchi
+   Assignment Number: 18
+
+   Topic: Big integer plus operation
+
+   file name: main.c
+
+   Date: July 2nd, 2016
+
+   Background:
+
+   Objective: 
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Hello world!\n");
+	exit(EXIT_SUCCESS);
+}
