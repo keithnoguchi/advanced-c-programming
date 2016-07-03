3d79f5eb683023e9757e859b4270443aea7ca055
Kei Nohguchi <kei@nohguchi.com>
2016-07-03 03:21:20 -0700

    18/main.c: Initial version of the addition logic/implementation

--- 18/main.c
+++ 18/main.c
@@ -36,6 +36,7 @@
 #include <assert.h>
 
 static const int max_number_of_numbers = 2;
+static const int reading_unit_size = 4;
 #define DEBUG
 #ifdef DEBUG
 static const char separator = ',';
@@ -121,12 +122,18 @@ static struct number *pop_number(struct number **head)
 static int __print_number(FILE *os, const struct number *const number,
 		const bool is_head)
 {
-	int ret;
+	int ret = 0;
 
-	if (is_head)
-		ret = xprintf(os, "%d", number->value);
-	else
-		ret = xprintf(os, "%04d", number->value);
+	if (is_head) {
+		if ((number->value / 10) == 0)
+			ret += xprintf(os, "   ");
+		else if ((number->value / 100) == 0)
+			ret += xprintf(os, "  ");
+		else if ((number->value / 1000) == 0)
+			ret += xprintf(os, " ");
+		ret += xprintf(os, "%d", number->value);
+	} else
+		ret += xprintf(os, "%04d", number->value);
 
 	if (number->next != NULL)
 		ret += xprintf(os, "%c", separator);
@@ -140,14 +147,15 @@ static struct number *alloc_number(char *buf)
 	struct number *head = NULL;
 	char *ptr;
 
-	/* Get every four digit, from the botton.  */
-	for (ptr = buf + size - 4; ptr >= buf; ptr -= 4) {
+	/* Get every four digit, from the least significat part.  */
+	for (ptr = buf + size - reading_unit_size; ptr >= buf;
+			ptr -= reading_unit_size) {
 		head = push_number(head, new_number(ptr));
 		*ptr = '\0';
 	}
 
 	/* Most significant part. */
-	if (ptr + 4 > buf)
+	if (ptr + reading_unit_size > buf)
 		head = push_number(head, new_number(buf));
 
 	return head;
@@ -161,13 +169,22 @@ static void delete_number(struct number *head)
 		free_number(number);
 }
 
+static struct number *__copy_number(const struct number *const from)
+{
+	struct number *to;
+
+	to = __new_number(from->value);
+	to->position = from->position;
+
+	return to;
+}
+
 static struct number *copy_number(const struct number *const from_head)
 {
 	const struct number *from = from_head;
 	struct number *head, *prev;
 
-	head = prev = __new_number(from->value);
-	prev->position = from->position;
+	head = prev = __copy_number(from);
 	while ((from = from->next)) {
 		prev->next = __new_number(from->value);
 		prev->next->position = from->position;
@@ -176,20 +193,62 @@ static struct number *copy_number(const struct number *const from_head)
 	return head;
 }
 
+static struct number *__add_number(struct number *to_head,
+		const struct number *const from_head)
+{
+	int max_position = to_head->position > from_head->position ?
+		to_head->position : from_head->position;
+	const struct number *from = from_head;
+	struct number *to = to_head;
+	struct number *head, *prev;
+	int i;
+
+	head = prev = to;
+	for (i = max_position; i >= 0; i--) {
+		if (to->position == from->position) {
+			to->value += from->value;
+			if (to->value >= 10000) {
+				prev->value += to->value / 10000;
+				to->value %= 10000;
+			}
+			prev = to;
+			from = from->next;
+			to = to->next;
+		} else if (from->position == i) {
+			if (head == to) {
+				head = prev = __copy_number(from);
+			} else {
+				prev->next = __copy_number(from);
+				prev = prev->next;
+			}
+			prev->next = to;
+			from = from->next;
+		} else if (to->position == i)
+			to = to->next;
+	}
+
+	return to_head;
+}
+
 static struct number *add_number(struct number *to,
 				const struct number *const from)
 {
 	if (to == NULL)
 		return copy_number(from);
 	else
-		return to;
+		return __add_number(to, from);
 }
 
-static int print_number(FILE *os, const struct number *const head)
+static int print_number(FILE *os, const struct number *const head,
+		const size_t max_position)
 {
 	const struct number *number;
 	bool is_head = true;
+	int offset;
 	int ret = 0;
+
+	for (offset = max_position - head->position; offset > 0; offset--)
+		xprintf(os, "%5c", ' ');
 
 	for (number = head; number != NULL; number = number->next) {
 		ret += __print_number(os, number, is_head);
@@ -199,17 +258,21 @@ static int print_number(FILE *os, const struct number *const head)
 	return ret;
 }
 
-static int get_numbers(FILE *is, struct number *nums[], const size_t size)
+static int get_numbers(FILE *is, struct number *nums[], const size_t size,
+		int *max_position)
 {
+	struct number *num;
 	char buf[BUFSIZ];
 	int i = 0;
 
+	*max_position = 0;
 	while (fscanf(is, "%s\n", buf) != EOF) {
-		nums[i++] = alloc_number(buf);
+		num = nums[i++] = alloc_number(buf);
+		if (*max_position < num->position)
+			*max_position = num->position;
 		if (i == size)
 			break;
 	}
-
 	return i;
 }
 
@@ -227,20 +290,30 @@ static void process(FILE *is, FILE *os)
 {
 	struct number *numbers[max_number_of_numbers];
 	struct number *result = NULL;
+	int max_position;
 	int i, num;
 
 	print_title(os);
 
-	num = get_numbers(is, numbers, max_number_of_numbers);
+	num = get_numbers(is, numbers, max_number_of_numbers,
+			&max_position);
 
 	for (i = 0; i < num; i++) {
-		print_number(os, numbers[i]);
-		result = add_number(result, numbers[i]);
 		if (i + 1 < num)
-			xprintf(os, " + ");
+			xprintf(os, "  ");
+		else
+			xprintf(os, "+ ");
+
+		print_number(os, numbers[i], max_position);
+		result = add_number(result, numbers[i]);
+		xprintf(os, "\n");
 	}
-	xprintf(os, " = ");
-	print_number(os, result);
+
+	xprintf(os, "--");
+	for (i = 0; i <= max_position; i++)
+		xprintf(os, "-----");
+	xprintf(os, "\n  ");
+	print_number(os, result, max_position);
 	xprintf(os, "\n");
 
 	for (i = 0; i < num; i++)
